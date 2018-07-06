e67d6af7bdcb0b999135635133f81bf9eb9f52c7
hircumg <hircumg@gmail.com>
2018-07-06 10:22:21 +0300

    added comments

--- scripts/laser_example.py
+++ scripts/laser_example.py
@@ -9,33 +9,39 @@ import numpy as np
 
 
 if __name__ == "__main__":
+
+    # initialize robot
     robot = Robot()
 
-    laser = robot.getLaser()
-    dir = robot.getDirection()
-    enc = robot.getEncoders()
-    print("laser type: %s"% laser)
-    print("Directon of robot: %s and values of encoders: left: %s, right: %s"%(dir,enc.get("left"),enc.get("right")))
 
+    # get laser values
+    laser = robot.getLaser()
+
+    # get values of laser and remove some extreme values because laser see himself
     laser = laser.get('values')[40:len(laser.get('values'))-40]
     print(laser)
-    print(len(laser))
 
-
+    # how close robot could go to the obstacles
     threshold = 0.5
+
     robot.setVelosities(0.3, 0)
-    while(1):
+
+    while True:
+
         robot.setVelosities(0.3, 0)
         laser = robot.getLaser()
         laser = laser.get('values')[40:len(laser.get('values')) - 40]
+
+        # find minimum value if laser values
         min = 5
         for i in laser:
             if i < min:
                 min = i
         print("h = %s"%(min))
 
+        # Check should or should not robot turn to avoid obstacle
         if (min < threshold):
-            print("wall")
+            print("There's an obstacle near")
             robot.setVelosities(0,0.3)
             while(min <= threshold):
                 laser = robot.getLaser()
@@ -45,11 +51,12 @@ if __name__ == "__main__":
                     if i < min:
                         min = i
                 print("h = %s" % (min))
-
                 robot.sleep(0.5)
 
             print("no wall")
             robot.setVelosities(0.3, 0)
-        robot.sleep(0.01)
+
+        # some delay for don't overload computation
+        robot.sleep(0.005)
 
     exit(1)
